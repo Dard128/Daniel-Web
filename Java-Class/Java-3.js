3d4b1fbf9b697fe4878b35fc691a63d5acd401d5
문효성 <moonhyoseong@JYP.local>
2021-11-18 09:25:14 +0900

    homework3

---
+++ Java-Class/Java-3.js
@@ -0,0 +1,31 @@
+
+let season = 'summer';
+
+if (season == 'spring') {
+    console.log("spring");
+} else if(season == 'summer') {
+    console.log('summer');
+}else if(season == 'fall') {
+    console.log('fall');
+}else if(season == 'winter') {
+    console.log('winter');
+}
+
+let gpa = 'A'
+let score;
+
+switch (gpa) {
+    case 'A':
+        console.log(100);
+        break;
+
+    case "B":
+        console.log(90);
+        break;
+
+    case 'C':
+        console.log(80);
+        break;
+            
+  
+}
---
+++ Java-Class/java-2.js
@@ -0,0 +1,36 @@
+let hungerlevel = 7;
+
+
+
+if (hungerlevel > 7) {
+
+    console.log("Time to eat");
+    
+    
+}else{
+    console.log("We can eat later!");
+}
+
+let mood = 'sleepy';
+let tirednesslevel = 6;
+
+if(mood === 'sleepy' && tirednesslevel > 6 ) {
+    console.log("time to sleep");
+
+}else{
+    console.log("not time yet");
+}
+
+let tool = 'pen';
+let writingUtensil = tool || 'pen' ;
+
+
+    console.log(`The dlafkjd;lfkj ${writingUtensil} sakfjalsjk`);
+
+let NightTime = true;
+if (NightTime) {
+    console.log("Turn on the light");
+}else{
+    console.log("Turn off the light");
+}
+isCorrect ? console.log("turn on the lights") : console.log("Turn off the lights");
